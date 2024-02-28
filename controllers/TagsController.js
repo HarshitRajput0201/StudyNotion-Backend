518cd2e22f1e7a72ec177398a12575189e35ec8c
HarshitRajput0201 <harshitrajput0201@gmail.com>
2024-02-28 19:22:23 +0530

    updated TagsPageDetails in TagsController.js

--- controllers/TagsController.js
+++ controllers/TagsController.js
@@ -47,4 +47,34 @@ exports.showAllTags = async (req, res) => {
             message: 'Tag Cannot Be Created'
         });
     }
+};
+
+exports.categoryPageDetails = async (req, res) => {
+    try {
+        const { categoryId } = req.body;
+        const selectedCategory = await categoryId.findById(categoryId).populate('courses').exec();
+        if(!selectedCategory){
+            return res.status(401).json({
+                success: false,
+                message: 'Category Not Found'
+            });
+        }
+
+        const differentCategory = await categoryId.find({ _id: { $ne: categoryId } }).populate('courses').exec();
+        return res.status(200).json({
+            success: true,
+            message: 'Showing Category Details',
+            data: {
+                differentCategory,
+                selectedCategory
+            }
+        });
+    } 
+    catch (error) {
+        console.log(error);
+        return res.status(401).json({
+            success: false,
+            message: 'Cannot Get Category Page Details'
+        });
+    }
 }
