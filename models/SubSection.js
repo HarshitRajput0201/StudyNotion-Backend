7ee97fc51408f5b2fc977a639255b08e40f2e810
HarshitRajput0201 <harshitrajput0201@gmail.com>
2024-02-29 22:11:48 +0530

    modify SubSection.js

--- models/SubSection.js
+++ models/SubSection.js
@@ -16,4 +16,4 @@ const subSectionSchema = new mongoose.Schema({
     }
 });
 
-module.exports = mongoose.Schema('SubSection', subSectionSchema);
+module.exports = mongoose.model('SubSection', subSectionSchema);
