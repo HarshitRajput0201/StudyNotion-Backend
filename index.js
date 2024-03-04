1b6b26a0e9253f5ecc05851ca5061cfd0b082ee7
HarshitRajput0201 <harshitrajput0201@gmail.com>
2024-03-04 22:04:35 +0530

    added index.js

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+
+const userRoutes = require('./routes/UserRoute');
+const profileRoutes = require('./routes/ProfileRoute');
+const paymentRoutes = require('./routes/PaymentRoute');
+const courseRoutes = require('./routes/CourseRoute');
+
+const database = require('./config/database');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const { cloudinaryConnect } = require('./config/cloudinary');
+const fileUpload = require('express-file-upload');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const PORT = process.env.PORT || 4000;
+
+database.connect();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+    cors({
+        origin: '*',
+        credentials: true
+    })
+);
+app.use(
+    fileUpload({
+        usetempFiles: true,
+        tempFileDir: '/tmp'
+    })
+);
+
+cloudinaryConnect();
+
+app.use('/api/users', userRoutes);
+app.use('/api/profiles', profileRoutes);
+app.use('/api/payments', paymentRoutes);
+app.use('/api/courses', courseRoutes);
+
+app.get('/', (req, res) => {
+    res.json({
+        success: true,
+        message: 'Server Is Running'
+    })
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
