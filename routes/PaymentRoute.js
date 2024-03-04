e91be1bc1bb9bf9d5e92d7de1a4f542c0b81f98e
HarshitRajput0201 <harshitrajput0201@gmail.com>
2024-03-04 20:00:08 +0530

    added ProfileRoute.js

--- routes/PaymentRoute.js
+++ routes/PaymentRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express('router');
+const router = express.Router();
 
 const { capturePayment, verifySignature } = require('../controllers/PaymentController');
 const { auth, isStudent } = require('../middlewares/Authentication');
