f79fde936f5de2d9973c46ebf16543d4f1934a20
HarshitRajput0201 <harshitrajput0201@gmail.com>
2024-03-04 12:53:00 +0530

    Modify SubSectionController.js

--- controllers/SubSectionController.js
+++ controllers/SubSectionController.js
@@ -5,9 +5,9 @@ require('dotenv').config();
 
 exports.createSubSection = async (req, res) => {
     try {
-        const { sectionId, title, timeDuration, description } = req.body;
+        const { sectionId, title, description } = req.body;
         const video = req.files.videoFile;
-        if(!sectionId || !title || !timeDuration || !description || !video){
+        if(!sectionId || !title || !description || !video){
             return res.status(401).json({
                 success: false,
                 message: 'Something is Missing in Subsection Entry'
@@ -18,7 +18,7 @@ exports.createSubSection = async (req, res) => {
         const subSectionDetails = await SubSection.create(
                                                     {
                                                         title: title, 
-                                                        timeDuration: timeDuration, 
+                                                        timeDuration: `${uploadDetails.duration}`, 
                                                         description: description,
                                                         videoUrl: uploadDetails.secure_url
                                                     }
@@ -33,10 +33,11 @@ exports.createSubSection = async (req, res) => {
                                                         {
                                                             new: true
                                                         }
-        );
+        ).populate('subSection');
         return res.status(200).json({
             success: true,
-            message: 'SubSection Created'
+            message: 'SubSection Created',
+            updateSection
         });
     } 
     catch (error) {
@@ -46,4 +47,74 @@ exports.createSubSection = async (req, res) => {
             message: 'SubSection Not Created'
         });
     }
-}
+};
+
+exports.updateSubSection = async (req, res) => {
+    try {
+        const { sectionId, title, description } = req.body;
+        const subSection = await SubSection.findById(sectionId);
+        if(!subSection){
+            return res.status(401).json({
+                success: false,
+                message: 'SubSection Not Found'
+            });
+        }
+
+        if(title !== undefined){
+            subSection.title = title;
+        }
+        if(description!== undefined){
+            subSection.description = description;
+        }
+        if(req.files && req.files.video !== undefined){
+            const video = req.files.video;
+            const uploadDetails = await uploadImageToCloudinary(video, process.env.FOLDER_NAME);
+            subSection.videoUrl = uploadDetails.secure_url;
+            subSection.timeDuration = `${uploadDetails.duration}`;
+        }
+        await subSection.save();
+        return res.status(200).json({
+            success: true,
+            message: 'SubSection Updated'
+        });
+    } 
+    catch (error) {
+        console.log(error);
+        return res.status(401).json({
+            success: false,
+            message: 'SubSection Not Updated'
+        });
+    }
+};
+
+exports.deleteSubSection = async (req, res) => {
+    try {
+        const { subSectionId, sectionId } = req.body;
+        await Section.findByIdAndUpdate(
+            { _id: sectionId },
+            { 
+                $pull : {
+                    subSectionId: subSectionId
+                }
+            }
+        )
+        const subSection = await SubSection.findByIdAndDelete({ _id: subSectionId });
+        if(!subSection){
+            return res.status(401).json({
+                success: false,
+                message: 'SubSection Not Found'
+            });
+        }
+        return res.status(200).json({
+            success: true,
+            message: 'SubSection Deleted'
+        });
+    } 
+    catch (error) {
+        console.log(error);
+        return res.status(401).json({
+            success: false,
+            message: 'SubSection Not Deleted'
+        });
+    }
+};
